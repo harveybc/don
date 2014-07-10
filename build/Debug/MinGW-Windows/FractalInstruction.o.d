e0088fc7df7b738131a78e5a74ca71419e0a4348
harveybc <harveybc@ingeni-us.com>
2014-07-10 01:28:35 -0500

    Revert "Descentralized AI expert repository and app interface"
    This reverts commit 6852cc48fe6fc4b8ee1f59b534af8edd66e5e33c.

--- build/Debug/MinGW-Windows/FractalInstruction.o.d
+++
@@ -1,4 +0,0 @@
-build/Debug/MinGW-Windows/FractalInstruction.o: FractalInstruction.cpp \
- FractalInstruction.h
-
-FractalInstruction.h:
--- build/Debug/MinGW-Windows/cli_storage.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/MinGW-Windows/cli_storage.o: cli_storage.cpp cli_storage.h
-
-cli_storage.h:
