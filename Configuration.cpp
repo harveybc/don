d053310003d2c9e0fdaa9261d9f2ef4dce80e091
Harvey Bastidas <harveybc@ingeni-us.com>
2015-08-30 05:43:51 -0500

    Doxygen documentation update

---
+++ Configuration.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Configuration.cpp
+ * Author: harveybc
+ * 
+ * Created on 30 de agosto de 2015, 03:59 AM
+ */
+
+#include "Configuration.h"
+
+Configuration::Configuration() {
+}
+
+Configuration::Configuration(const Configuration& orig) {
+}
+
+Configuration::~Configuration() {
+}
+
