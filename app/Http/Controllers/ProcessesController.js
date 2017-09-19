2033386dceb6274536eba48f53d76cea9b8f8dc8
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-09-18 20:39:30 -0500

    Processes action Controller and Route for HEAD
    Processes Controller and Route for HEAD

--- app/Http/Controllers/ProcessesController.js
+++ app/Http/Controllers/ProcessesController.js
@@ -1,7 +1,19 @@
 'use strict'
 
 class ProcessesController {
+    * headList(request, response) {
+        const characters = {
+            "num_results": "3",
+            "results":
+                    [
+                        {"id": "1", "model_id": "4", "performance": "0.75543", "date_optimum": "2017-09-02 05:22:31"},
+                        {"id": "2", "model_id": "5", "performance": "0.81513", "date_optimum": "2017-09-02 05:20:12"},
+                        {"id": "3", "model_id": "2", "performance": "0.84784", "date_optimum": "2017-09-02 05:11:54"}
+                    ]
+        }
 
+        yield response.sendView('welcome', {characters: characters})
+    }
 }
 
 module.exports = ProcessesController
--- app/Http/routes.js
+++ app/Http/routes.js
@@ -17,4 +17,20 @@
 
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.route('/', 'HEAD', 'ProcessesController.headList')
+
+
+// Route.get('/', 'ListController.show')
+// Route.get('/login', 'AuthController.index')
+// Route.post('/login', 'AuthController.login')
+
+// Route.get('/register', 'RegisterController.index')
+// Route.post('register', 'RegisterController.doRegister')
+
+
+// Route.put('/api/:resource', 'CrudController.update') //optional 
+// Route.get('/api/:resource/grid', 'CrudController.grid') //the grid configurations for the list grid view 
+// Route.get('/api/:resource/form', 'CrudController.form') //the form configurations for create 
+// Route.get('/api/:resource/:id/form', 'CrudController.form') // the form configurations for edit 
+// Route.resource('/api/:resource', 'CrudController') //CRUD for resources 
+// Route.resource('/api/:parent/:parentId/:resource', 'CrudController') //CRUD for netsted resource (In Progress...) 
