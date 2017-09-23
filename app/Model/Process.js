ab68795fe6c4fe44c6975b0df461f5f10f021d18
Ubuntu <ubuntu@colkart.com>
2017-09-23 19:52:11 +0000

    Process and ProcessMetadata model creation

---
+++ app/Model/Process.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const Lucid = use('Lucid')
+
+class Process extends Lucid {
+
+}
+
+module.exports = Process
---
+++ app/Model/ProcessMetadata.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const Lucid = use('Lucid')
+
+class ProcessMetadata extends Lucid {
+
+}
+
+module.exports = ProcessMetadata
