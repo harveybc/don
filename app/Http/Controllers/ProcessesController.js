129b118cb8b4fb7ab6306d2d3bf365f51ddc77e9
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-09-23 23:48:03 -0500

    njk template for process modified

--- app/Http/Controllers/ProcessesController.js
+++ app/Http/Controllers/ProcessesController.js
@@ -11,7 +11,7 @@ class ProcessesController {
                         {"process_id": "3", "model_id": "2", "performance": "0.84784", "date_optimum": "2017-09-02 05:11:54"}
                     ]
         }
-        yield response.sendView('master_old', {metadata_list: metadata_list, id: 3})
+        yield response.sendView('headList_JSON', {metadata_list: metadata_list, id: 3})
     }
 }
 
