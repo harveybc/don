c01f15f0412901a3097964d7a4172fa55573557c
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-09-23 22:38:06 -0500

    HEAD processes view
    HEAD processes view

--- app/Http/Controllers/ProcessesController.js
+++ app/Http/Controllers/ProcessesController.js
@@ -6,13 +6,14 @@ class ProcessesController {
             "num_results": "3",
             "results":
                     [
-                        {"id": "1", "model_id": "4", "performance": "0.75543", "date_optimum": "2017-09-02 05:22:31"},
-                        {"id": "2", "model_id": "5", "performance": "0.81513", "date_optimum": "2017-09-02 05:20:12"},
-                        {"id": "3", "model_id": "2", "performance": "0.84784", "date_optimum": "2017-09-02 05:11:54"}
+                        {"process_id": "1", "model_id": "4", "performance": "0.75543", "date_optimum": "2017-09-02 05:22:31"},
+                        {"process_id": "2", "model_id": "5", "performance": "0.81513", "date_optimum": "2017-09-02 05:20:12"},
+                        {"process_id": "3", "model_id": "2", "performance": "0.84784", "date_optimum": "2017-09-02 05:11:54"}
                     ]
         }
-        yield response.sendView('headList', {metadata_list: metadata_list})
+        yield response.sendView('headList', {metadata_list: metadata_list, id: 3})
     }
 }
 
 module.exports = ProcessesController
+    
