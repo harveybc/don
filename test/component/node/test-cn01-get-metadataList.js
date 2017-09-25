25d4808e56db789d37ae992de70ab90af18e984c
Ubuntu <ubuntu@colkart.com>
2017-09-25 12:38:22 +0000

    Test for continuous integration with buddy

--- test/component/node/test-cn01-get-metadataList.js
+++ test/component/node/test-cn01-get-metadataList.js
@@ -4,7 +4,7 @@ var request = require('request');
 var description = 'GET processes metadataList request';
 var parameters = 'app_id=forex&public_key=PUB_KEY&model_id=10&min_performance=0.5&max_results=100&xml=false';
 var endpoint = 'http://dev.ingeni-us.com:3338/processes/metadata?';
-var expected_response = '{"jsonrpc": "2.0", "result": [{"process_id":1,"model_id":4,"performance":0.75543,"date_optimum":"2017-09-02 05:22:31"},{"process_id":2,"model_id":5,"performance":0.81513,"date_optimum":"2017-09-02 05:20:12"},{"process_id":3,"model_id":2,"performance":0.84784,"date_optimum":"2017-09-02 05:11:54"}], "id":3}';
+var expected_response = '{"jsonrpc": "1.0", "result": [{"process_id":1,"model_id":4,"performance":0.75543,"date_optimum":"2017-09-02 05:22:31"},{"process_id":2,"model_id":5,"performance":0.81513,"date_optimum":"2017-09-02 05:20:12"},{"process_id":3,"model_id":2,"performance":0.84784,"date_optimum":"2017-09-02 05:11:54"}], "id":3}';
 
 it(description, function(done) {
     request.get(endpoint+parameters , function(error, response, body) {
