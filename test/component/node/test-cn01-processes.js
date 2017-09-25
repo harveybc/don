7b121952bd574bae7cc41acef7908212745d0d9c
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-09-25 15:51:00 -0500

    test

--- test/component/node/test-cn01-processes.js
+++ test/component/node/test-cn01-processes.js
@@ -28,7 +28,7 @@ describe('Processes Collection', function() {
     parameters = 'app_id=forex&public_key=PUB_KEY&xml=false';
     endpoint = 'http://dev.ingeni-us.com:3338/processes/metadata/1?';
     expected_response = '{"jsonrpc": "2.0", "result": {"process_id":1,"model_id":4,"performance":0.75543,"date_optimum":"2017-09-02 05:22:31"}, "id":3}';
-    // Assesment 
+    // Assesment  
     it(description, function(done) {
         // Makes request to the endpoint
         request.get(endpoint+parameters , function(error, response, body) {
