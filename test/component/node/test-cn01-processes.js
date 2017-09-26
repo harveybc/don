138217c91b85f79b52a21478bc8e12ce5833a130
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-09-26 09:57:02 -0500

    test

--- test/component/node/test-cn01-processes.js
+++ test/component/node/test-cn01-processes.js
@@ -21,7 +21,8 @@ describe('Processes Collection', function() {
             expect(JSON.stringify(parsed_body)).to.equal(JSON.stringify(parsed_expected_response));
             done();
         });
-    });
+    }); 
+    
     // Test CN01-2:  GET processes/metadata/<id> request (metadataItem)
     // Configuration
     description = 'CN01-2: GET processes/metadata/<id> (MetadataItem)';
@@ -41,9 +42,9 @@ describe('Processes Collection', function() {
     });
     // Test CN01-3:  GET processes (GetList)
     // Configuration
-    description = 'CN01-3: GET processes (GetList)';
+    description = 'CN01-3: GET processes/item (GetList)';
     parameters = 'app_id=1&public_key=PUB_KEY&model_id=10&min_performance=0.5&max_results=100&xml=false';
-    endpoint = 'http://dev.ingeni-us.com:3338/processes?';
+    endpoint = 'http://dev.ingeni-us.com:3338/processes/item?';
     expected_response = '{"jsonrpc": "2.0", "result": [{"id": 1, "name": "Test process 1", "description": "Test process 1 -  stub testing", "creator_key": "CREATOR_KEY", "date": "2017-09-02 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009},            {"id": 2, "name": "Test process 2", "description": "Test process 2 -  stub testing", "creator_key": "CREATOR_KEY", "date": "2017-09-02 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009},            {"id": 3, "name": "Test process 3", "description": "Test process 3 -  stub testing", "creator_key": "CREATOR_KEY", "date": "2017-09-02 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009}], "id":3}';
     // Assesment
     it(description, function(done) {
@@ -56,9 +57,9 @@ describe('Processes Collection', function() {
     });
     // Test CN01-4:  GET processes<id> request (GetItem)
     // Configuration
-    description = 'CN01-4: GET processes/<id> (GetItem)';
+    description = 'CN01-4: GET processes/item/<id> (GetItem)';
     parameters = 'app_id=1&public_key=PUB_KEY&xml=false';
-    endpoint = 'http://dev.ingeni-us.com:3338/processes/1?';
+    endpoint = 'http://dev.ingeni-us.com:3338/processes/item/1?';
     expected_response = '{"jsonrpc": "2.0", "result": {"id": 1, "name": "Test process 1", "description": "Test process 1 -  stub testing", "creator_key": "CREATOR_KEY", "date": "2017-09-02 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009}, "id":3}';
     // Assesment  
     it(description, function(done) { 
