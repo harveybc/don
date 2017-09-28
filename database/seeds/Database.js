e3350b968f2966f19706bc0229a8a9ba880278e7
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-09-27 21:01:27 -0500

    Added Seed for test data
    Added Seed for test data

--- database/seeds/Database.js
+++ database/seeds/Database.js
@@ -14,10 +14,20 @@
 
 // const Factory = use('Factory')
 
-class DatabaseSeeder {
+// usa modelo de Procesos
+const Process = use(App/Model/Processes)
 
+class DatabaseSeeder {
+    
   * run () {
+      var processArray=[
+            {"id": 1, "name": "Test process 1", "description": "Test process 1 -  stub testing", "creator_key": "CREATOR_KEY", "date": "2017-09-02 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009},
+            {"id": 2, "name": "Test process 2", "description": "Test process 2 -  stub testing", "creator_key": "CREATOR_KEY", "date": "2017-09-02 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009},
+            {"id": 3, "name": "Test process 3", "description": "Test process 3 -  stub testing", "creator_key": "CREATOR_KEY", "date": "2017-09-02 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009}
+        ]
     // yield Factory.model('App/Model/User').create(5)
+    yield Process.createMany(processArray) 
+      
   }
 
 }
