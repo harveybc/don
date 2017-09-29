8e1debfd2a47715dec54aab68c9ec4ec990b81c6
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-09-28 21:23:14 -0500

    test

--- test/component/node/test-cn01-processes-db.js
+++ test/component/node/test-cn01-processes-db.js
@@ -9,7 +9,7 @@ describe('Processes Collection', function () {
         // Configuration
         var parameters = 'app_id=1&public_key=PUB_KEY&model_id=10&min_performance=0.5&max_results=100&xml=false';
         var endpoint = 'http://dev.ingeni-us.com:3338/db/processes/metadata?';
-        var expected_response = '{"jsonrpc": "2.0", "result": [{"id":1, "updated_at": "2017-09-29 02:01:46", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009},{"id":2, "updated_at": "2017-09-29 02:01:46", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009},{"id":3, "updated_at": "2017-09-29 02:01:46", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009}], "id":3}';
+        var expected_response = '{"jsonrpc": "2.0", "result": [{"id":1, "updated_at": "2017-09-29 02:17:57", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009},{"id":2, "updated_at": "2017-09-29 02:17:58", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009},{"id":3, "updated_at": "2017-09-29 02:17:58", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009}], "id":3}';
         // Assesment
         request.get(endpoint + parameters, function (error, response, body) {
             parsed_body = JSON.parse(body);
@@ -24,7 +24,7 @@ describe('Processes Collection', function () {
         // Configuration
         var parameters = 'app_id=1&public_key=PUB_KEY&xml=false';
         var endpoint = 'http://dev.ingeni-us.com:3338/db/processes/metadata/1?';
-        var expected_response = '{"jsonrpc": "2.0", "result": {"id":1, "updated_at": "2017-09-03 05:22:31", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009}, "id":3}';
+        var expected_response = '{"jsonrpc": "2.0", "result": {"id":1, "updated_at": "2017-09-29 02:17:57", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009}, "id":3}';
        // metadata='{"jsonrpc": "2.0", "result": {"id": 1, "name": "Test process 1", "description": "Test process 1 -  stub testing", "created_at":"2017-09-02 05:22:31", "created_at": "2017-09-02 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2}, "id":3}'
         // Assesment  
         request.get(endpoint + parameters, function (error, response, body) {
