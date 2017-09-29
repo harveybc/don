c23781417be3101faff8a0f356fca7b6f232bd62
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-09-28 19:50:31 -0500

    Added DB Tests

--- app/Http/Routes/DB/Processes.js
+++ app/Http/Routes/DB/Processes.js
@@ -5,16 +5,16 @@
 const Route = use('Route')
 // PROCESSES COLLECTION MANAGEMENT:
 // MetadataList: get a list of processes' metadata 
-Route.get('/db/processes/metadata', 'ProcessesDBController.MetadataList')
+Route.get('/db/processes/metadata', 'DB/ProcessesDBController.MetadataList')
 // MetadataItem: get a list of a process' metadata
-Route.get('/db/processes/metadata/:id', 'ProcessesDBController.MetadataItem')
+Route.get('/db/processes/metadata/:id', 'DB/ProcessesDBController.MetadataItem')
 // GetList: get a list of processes
-Route.get('/db/processes', 'ProcessesDBController.GetList')
+Route.get('/db/processes', 'DB/ProcessesDBController.GetList')
 // GetItem: get a process
-Route.get('/db/processes/:id', 'ProcessesDBController.GetItem')
+Route.get('/db/processes/:id', 'DB/ProcessesDBController.GetItem')
 // CreateItem: create a process
-Route.post('/db/processes', 'ProcessesDBController.CreateItem')
+Route.post('/db/processes', 'DB/ProcessesDBController.CreateItem')
 // DeleteItem: Deletes a process
-Route.delete('/db/processes/:id', 'ProcessesDBController.DeleteItem')
+Route.delete('/db/processes/:id', 'DB/ProcessesDBController.DeleteItem')
 // EmptyCollection: Deletes all processes in a collection for which the user is admin
-Route.delete('/db/processes', 'ProcessesDBController.EmptyCollection')
+Route.delete('/db/processes', 'DB/ProcessesDBController.EmptyCollection')
