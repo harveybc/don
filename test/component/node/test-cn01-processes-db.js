6c3d93fe0dd032269c7723123f0e713875598e47
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-10-07 07:58:44 -0500

    test

--- test/component/node/test-cn01-processes-db.js
+++ test/component/node/test-cn01-processes-db.js
@@ -9,7 +9,7 @@ describe('Processes Collection', function () {
         // Configuration
         var parameters = 'app_id=1&public_key=PUB_KEY&model_id=10&min_performance=0.5&max_results=100&xml=false';
         // TODO: Cambiar STUB por DB
-        var endpoint = 'http://dev.ingeni-us.com:3338/stub/processes/metadata?';
+        var endpoint = 'http://dev.ingeni-us.com:3338/db/processes/metadata?';
         var expected_response = '{"jsonrpc": "2.0", "result": [{"id":1, "updated_at": "2017-09-03 05:22:31", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009},{"id":2, "updated_at": "2017-09-04 05:22:31", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009},{"id":3, "updated_at": "2017-09-05 05:22:31", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009}], "id":3}';
         // Assesment
         request.get(endpoint + parameters, function (error, response, body) {
@@ -25,7 +25,7 @@ describe('Processes Collection', function () {
         // Configuration
         var parameters = 'app_id=1&public_key=PUB_KEY&xml=false';
         // TODO: Cambiar STUB por DB
-        var endpoint = 'http://dev.ingeni-us.com:3338/stub/processes/metadata/1?';
+        var endpoint = 'http://dev.ingeni-us.com:3338/db/processes/metadata/1?';
         //var expected_response = '{"jsonrpc": "2.0", "result": {"id":1, "updated_at": "2017-09-29 02:17:57", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009}, "id":3}';
         var expected_response = '{"jsonrpc": "2.0", "result": {"id":1, "updated_at": "2017-09-03 05:22:31", "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "difficulty": 0.0009}, "id":3}';
         
@@ -45,7 +45,7 @@ describe('Processes Collection', function () {
         // Configuration
         var parameters = 'app_id=1&public_key=PUB_KEY&model_id=10&min_performance=0.5&max_results=100&xml=false';
         // TODO: Cambiar STUB por DB
-        var endpoint = 'http://dev.ingeni-us.com:3338/stub/processes?';
+        var endpoint = 'http://dev.ingeni-us.com:3338/db/processes?';
         var expected_response = '{"jsonrpc": "2.0", "result": [{"id": 1, "name": "Test process 1", "description": "Test process 1 -  stub testing", "created_at": "2017-09-02 05:22:31", "updated_at": "2017-09-03 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009},{"id": 2, "name": "Test process 2", "description": "Test process 2 -  stub testing", "created_at": "2017-09-03 05:22:31", "updated_at": "2017-09-04 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009},{"id": 3, "name": "Test process 3", "description": "Test process 3 -  stub testing", "created_at": "2017-09-04 05:22:31", "updated_at": "2017-09-05 05:22:31", "tags": ["p2p", "forex", "dqn"], "application_id": 2, "last_block_time": 86400, "last_block_size": 2048, "last_optimum_performance": 0.79983, "last_optimum_id": 1, "date_last_optimum": "2017-09-03 06:22:31", "desired_block_time": 85000, "desired_block_size": 0, "block_time_control": 1, "difficulty": 0.0009}], "id":3}';
         // Assesment
         request.get(endpoint + parameters, function (error, response, body) {
