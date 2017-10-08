27feed312c9583809403b69df2ddc44329be84a4
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-10-08 15:53:56 -0500

    test

--- test/component/node/test-cn01-processes-db.js
+++ test/component/node/test-cn01-processes-db.js
@@ -86,7 +86,7 @@ describe('Processes Collection', function () {
         request.delete(endpoint + parameters, function (error, response, body) {
             resp = JSON.parse(body);
             // Verify if the result is equal to the expected response. 
-            expect(resp.result.delete_count).to.equal(1);
+            expect(resp).to.equal("TEST");
             done();
         });
     });
