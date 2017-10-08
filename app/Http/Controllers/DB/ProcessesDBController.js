50249c740cce19297e080859f199760214f135ba
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-10-07 23:10:59 -0500

    test

--- app/Http/Controllers/DB/ProcessesDBController.js
+++ app/Http/Controllers/DB/ProcessesDBController.js
@@ -15,7 +15,7 @@ class ProcessesDBController {
     /** @desc Returns the metadata for the <id> process */
     * MetadataItem(request, response) {
         const Database = use('Database')
-        const result = yield Database.select("id", "updated_at", "last_block_time", "last_block_size", "last_optimum_performance", "last_optimum_id", "date_last_optimum", "difficulty").from('processes').where('id',1)
+        const result = yield Database.select("id", "updated_at", "last_block_time", "last_block_size", "last_optimum_performance", "last_optimum_id", "difficulty").from('processes').where('id',1)
         const process_id = request.param('id')
         yield response.sendView('master_JSON', {result: result[0], request_id: 3})
     }
--- test/component/node/test-cn01-processes-db.js
+++ test/component/node/test-cn01-processes-db.js
@@ -12,7 +12,7 @@ describe('Processes Collection', function () {
         var endpoint = 'http://dev.ingeni-us.com:3338/db/processes/metadata?';
         // Assesment
         request.get(endpoint + parameters, function (error, response, body) {
-            resp = JSON.parse(response);
+            resp = JSON.parse(body);
             // In the database the third process should have a id=3
             expect(resp.result[2].id).to.equal(3);
             done();
