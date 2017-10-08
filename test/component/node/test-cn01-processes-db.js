60a77f4d049eb25ed13cf335e3f6c4057ca1b47a
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-10-07 22:56:12 -0500

    test

--- test/component/node/test-cn01-processes-db.js
+++ test/component/node/test-cn01-processes-db.js
@@ -12,7 +12,7 @@ describe('Processes Collection', function () {
         var endpoint = 'http://dev.ingeni-us.com:3338/db/processes/metadata?';
         // Assesment
         request.get(endpoint + parameters, function (error, response, body) {
-            resp = JSON.parse(body);
+            resp = JSON.parse(response);
             // In the database the third process should have a id=3
             expect(resp.result[2].id).to.equal(3);
             done();
@@ -26,7 +26,7 @@ describe('Processes Collection', function () {
         var endpoint = 'http://dev.ingeni-us.com:3338/db/processes/metadata/1?';
         // Assesment  
         request.get(endpoint + parameters, function (error, response, body) {
-            resp = JSON.parse(body);
+            resp = JSON.parse(response);
             // In the database the third process should have a id=1
             expect(resp.result[2].id).to.equal(1);
             done();
@@ -40,7 +40,7 @@ describe('Processes Collection', function () {
         var endpoint = 'http://dev.ingeni-us.com:3338/db/processes?';
         // Assesment
         request.get(endpoint + parameters, function (error, response, body) {
-            resp = JSON.parse(body);
+            resp = JSON.parse(response);
             // In the database the third process should have a id=3
             expect(resp.result[2].id).to.equal(3);
             done();
