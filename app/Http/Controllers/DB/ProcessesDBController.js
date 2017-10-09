b7c540348fb798738603c66489326a39fe9e5cc0
Harvey Bastidas <harveybc@users.noreply.github.com>
2017-10-09 00:10:39 -0500

    test

--- app/Http/Controllers/DB/ProcessesDBController.js
+++ app/Http/Controllers/DB/ProcessesDBController.js
@@ -53,7 +53,7 @@ class ProcessesDBController {
     * DeleteItem(request, response) {
         const Database = use('Database')
         const process_id = request.param('id')
-        const deleted_count = yield Database.table.where('id',process_id).delete();
+        const deleted_count = yield Database.table('processes').where('id',process_id).delete();
 //        const result = {"delete_count": count_deleted};
         yield response.sendView('master_JSON', {result: 1, request_id: 3})
     }
