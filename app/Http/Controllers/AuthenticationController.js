38e2fc50db07b38ec3a4681cd1d18bba5324269b
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-04-15 05:25:07 -0500

    test parameters for flooding
    test parameters for flooding

--- app/Http/Controllers/AuthenticationController.js
+++ app/Http/Controllers/AuthenticationController.js
@@ -134,7 +134,7 @@ class AuthenticationController {
             yield response.sendView('master_JSON', {result: {"error": account_res, "code": 402}, request_id: 3});
         }
         // send response
-        yield response.sendView('master_JSON', {"result": result, "account_res": account_res, "request_id": 5});
+        yield response.sendView('master_JSON', {"result": account_res, "request_id": 5});
     }
     * updateItemQuery(request, response) {
         // generate parameters for query
