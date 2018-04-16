d0df75faf478c276223a59b20773fcc949181281
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-04-16 07:17:54 -0500

    test
    test

--- app/Http/Controllers/AccountingController.js
+++ app/Http/Controllers/AccountingController.js
@@ -56,7 +56,7 @@ class AccountingController {
                     }
             );
         }
-        var res = { new_ttl: new_ttl, num_found:num_found[0].counted,max_connections:max_c[0].max_connections, num_neighs:num_neighs, result:result, url:result[i].address + '/flooding',form: formData }
+        var res = { new_ttl: new_ttl, num_found:num_found[0].counted,max_connections:max_c[0].max_connections, num_neighs:num_neighs, result:result, url:result[0].address + '/flooding',form: formData }
         return res;
     }
     // Flooding: this method is called from the route /flooding and does AAA
