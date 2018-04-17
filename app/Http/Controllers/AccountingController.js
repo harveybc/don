c9d7f664da9f2f6dfd3922f5fac71c8dc45d74d4
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-04-16 20:41:09 -0500

    tmp
    tmp

--- app/Http/Controllers/AccountingController.js
+++ app/Http/Controllers/AccountingController.js
@@ -102,7 +102,7 @@ class AccountingController {
                 var A = use('App/Http/Controllers/AuthenticationController');
                 var a = new A();
                 if (m === 3) { // method: create
-                    const auth_res = yield * a.createItemQuery(JSON.parse(JSON.stringify(parameters_raw)));
+                    const auth_res = yield * a.createItemQuery(parameters_raw);
                     if (!auth_res) {
                         yield response.sendView('master_JSON', {result: {"error": auth_res, "code": 400}, request_id: 8});
                     }
@@ -342,7 +342,7 @@ class AccountingController {
              }
              }
              */
-            yield response.sendView('master_JSON', {result: num_found , request_id: 70});
+            yield response.sendView('master_JSON', {result: parameters_raw , request_id: 70});
         }
     }
 
