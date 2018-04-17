4ae8d33c231c4513608233feef1b93facd1cdd30
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-04-16 22:08:22 -0500

    test
    test

--- app/Http/Controllers/AccountingController.js
+++ app/Http/Controllers/AccountingController.js
@@ -103,8 +103,8 @@ class AccountingController {
                 var A = use('App/Http/Controllers/AuthenticationController');
                 var a = new A();
                 if (m === 3) { // method: create
-                    parameters_raw = parameters_raw.replace('/"{','{');
-                    parameters_raw = parameters_raw.replace('}"','}');
+                    parameters_raw = parameters_raw.replace(/"{/,"TEST");
+                    parameters_raw = parameters_raw.replace(/}"/,'}');
                     const auth_res = yield * a.createItemQuery(parameters_raw);
                     if (!auth_res) {
                         yield response.sendView('master_JSON', {result: {"error": auth_res, "code": 400}, request_id: 8});
