acd9e9f2272a21a2edbe2628d597f2139aedf00c
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-04-16 23:56:02 -0500

    test
    test

--- app/Http/Controllers/AccountingController.js
+++ app/Http/Controllers/AccountingController.js
@@ -99,7 +99,7 @@ class AccountingController {
             // SI NO EXISTIA ANTES EL MISMO HASH hace Flooding
             var result = yield * this.flood(c, m, d, username, url_params.pass_hash, parameters_raw, result_raw, hash, TTL);
             // Adiciona el registro de accounting original 
-            const account_res = yield * this.Account(collection, method, d, username, JSON.stringify(parameters_raw), result_raw, hash, false);
+            const account_res = yield * this.Account(collection, method, d, username, JSON.stringify(parameters_raw), JSON.stringify(result_raw), hash, false);
             if (!account_res) {
                 yield response.sendView('master_JSON', {result: {"error": account_res, "code": 402}, request_id: 10});
             } 
