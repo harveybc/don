0893fa4704ffa64d0f017305f2cc627629c4a24b
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-08-28 23:28:32 -0500

    test
    test

--- app/Http/Controllers/BlocksController.js
+++ app/Http/Controllers/BlocksController.js
@@ -362,9 +362,8 @@ class BlocksController {
             items: [
                 {attr: "id", title: "id", type: "number", width: 20},
                 {attr: "performance", title: "perf", type: "number", width: 70},
-                {attr: "block_hash", title: "block_hash", type: "text", width: 30},
-                {attr: "prev_hash", title: "prev_hash", type: "text", width: 30},
                 {attr: "created_at", title: "Created At", type: "text", width: 60}
+                {attr: "prev_hash", title: "prev_hash", type: "text", width: 30},
                 
             ]
         });
