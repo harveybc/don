69e96dd83af1a4a5aa3bb92696a064ca03ca48b3
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-09-02 19:18:44 -0500

    test
    test

--- app/Http/Controllers/ParametersController.js
+++ app/Http/Controllers/ParametersController.js
@@ -55,11 +55,16 @@ class ParametersController {
         var cond = false;
         // ic collection=parameters and method=create
         console.log("\nc_vars.block_time_control=", c_vars.block_time_control,
-                " c_vars.current_block_performance=", c_vars.current_block_performance, " c_vars.last_block_performance=", c_vars.last_block_performance, " c_vars.current_threshold=", c_vars.current_threshold);
+                " c_vars.current_block_performance=", c_vars.current_block_performance, 
+                " c_vars.last_block_performance=", c_vars.last_block_performance, 
+                " c_vars.current_threshold=", c_vars.current_threshold);
         // If block time control method is OPoW (det-model) and Performance>Perf_anterior_bloque+Last_block_threshold
+        console.log("\nverificando=");
+        
         if ((c_vars.block_time_control === 0) && (c_vars.current_block_performance > (float(c_vars.last_block_performance) + float(c_vars.current_threshold))))
         {
             cond = true;
+            console.log("\nverificando=");
         }
         else{
             console.log("\nBlock Creation Condition NOT met.");
