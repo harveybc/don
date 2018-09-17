e446dba65d94f262524d2df83001f7004a2b0cfd
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-09-16 23:33:32 -0500

    test
    test

--- app/Http/Controllers/ParametersController.js
+++ app/Http/Controllers/ParametersController.js
@@ -77,7 +77,8 @@ class ParametersController {
             console.log("\nprev_hash:", prev_hash);
             
             // lee los registros marcados para usar como contents
-            var contents_a = yield Database.select('id').from('accountings').where('block_hash', "0");
+            // var contents_a = yield Database.select('id').from('accountings').where('block_hash', "0");
+            var contents_a = [{test}];
             console.log("\nTest");
             
             var contents = contents_a.stringify();       
